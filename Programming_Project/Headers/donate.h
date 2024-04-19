e224cc6e4443ed8c08107cacd4b5effbc7d2318a
Miruna Mesarosiu <162413951+Miruna022@users.noreply.github.com>
2024-04-19 12:38:28 +0300

    Delete Programming_Project directory

--- Programming_Project/Headers/donate.h
+++
@@ -1,9 +0,0 @@
-#ifndef UNTITLED_DONATE_H
-#define UNTITLED_DONATE_H
-#include<stdbool.h>
-void donate_books(const char *file_name);
-void update_copies_in_csv(const char *file_name, const char *temp_file_name, const char *title, const char *author, int copies);
-bool book_exists(const char *file_name, const char *title, const char *author);
-void add_book_to_library(const char *file_name, const char *title, const char *author, int copies);
-
-#endif //UNTITLED_DONATE_H
--- Programming_Project/Headers/return.h
+++
@@ -1,7 +0,0 @@
-#ifndef UNTITLED_RETURN_H
-#define UNTITLED_RETURN_H
-
-void return_books_to_library(const char *loans_file, const char *library_file, const char *temporary_file, const char *name_borrower, const char *surname_borrower);
-void show_user_books(const char *loans_file, const char *name_borrower, const char *surname_borrower);
-void search_book(const char *file_name, const char *search);
-#endif //UNTITLED_RETURN_H
